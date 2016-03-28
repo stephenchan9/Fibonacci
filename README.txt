135875abff20b3b73d8949f267c3ca48e5d850ec
Stephen <stephen.chan9@yahoo.com>
2016-03-27 22:20:45 -0700

    Added files
    Added assignment files for the assignment 1 CPSC 240

---
+++ README.txt
@@ -0,0 +1,7 @@
+Stephen Chan
+Assignment 1 Fibonacci
+
+How to run the program.
+
+1. Add a breakpoint before the exit.
+2. Check Array values
---
+++ fibonacci.asm
@@ -0,0 +1,49 @@
+; Assignment 1 Fibonacci
+; Stephen Chan
+; 9419
+; CPSC 240 Assignment 1
+; Partner: Daniel Berumen
+; Creates a fibonacci array of values.
+
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess proto,dwExitCode:dword
+
+.data
+ array DWORD 1,47 dup(0)							; set array values to be 0
+ loop_count DWORD 47								; initialize loop counter to be 47
+
+.code
+;-------------------------------------------------------
+; generate_fibonacci proc USES eax ebx ecx
+; Generates fibonacci values and stores in an array.
+; Receives: ESI points to the array, ECX = count
+; Returns: nothing
+;------------------------------------------------------- 
+
+generate_fibonacci proc uses eax ebx ecx
+	L1:
+	add eax,ebx								;f(n-1)+f(n-2)
+	
+	add esi, TYPE esi						;Increment the index
+	mov [esi],eax							;Move the eax value into array
+
+	xchg ebx,eax							;Have eax be the F(n-2) value
+	
+	loop L1
+	ret
+generate_fibonacci endp
+
+main proc
+	mov esi, OFFSET array				; gets the memory address of the array into esi
+	mov ecx, loop_count					; Sets loop counter to the array size
+	
+	mov eax,0							; Set the initial value of eax to 0
+	mov ebx,1							; Set the initial value of ebx to 1
+
+	call generate_fibonacci				; Calls the procedure
+
+	invoke ExitProcess,0
+main endp
+end main
